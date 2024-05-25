08ad1d8e617a2b5a7ca9f136cc67b45c9dfe2718
Konrad Goldammer <konradgoldammer@gmail.com>
2024-05-25 15:45:52 +0200

    Add Mantine component library for React

--- platform/frontend/app/layout.tsx
+++ platform/frontend/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
