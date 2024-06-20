cf4079556e8ee03da82f20bf853dd5fcef6385c8
Konrad Goldammer <konradgoldammer@gmail.com>
2024-06-20 15:26:18 +0200

    Add mock data for testing

--- platform/frontend/app/components/types.ts
+++ platform/frontend/app/components/types.ts
@@ -6,3 +6,21 @@ export interface UserData {
   username?: string;
   email: string;
 }
+export interface ChallengeData {
+  title: string;
+  description: string;
+  thumbnail: string; // URL to image
+  participants: number;
+  creator: string;
+  deadlineTimestamp: number | null;
+  template: string; // URL to xai method template
+  model: string; // URL to ML model
+  dataset: string; // URL to dataset
+}
+export interface LeaderboardData {
+  ranking: Array<EvaluationData>; // low index = high rating
+}
+export interface EvaluationData {
+  user: string;
+  score: number;
+}
--- platform/frontend/app/components/utils.ts
+++ platform/frontend/app/components/utils.ts
@@ -1,9 +1,85 @@
 import { IconMail } from "@tabler/icons-react";
-import { AuthenticationOption } from "./types";
+import { AuthenticationOption, ChallengeData, LeaderboardData } from "./types";
 
 export const BASE_URL_API = "http://localhost:8000";
+
 export const NO_HEADER_PAGES = ["/login", "/register"]; // pages where the header should be hidden
+
 export const NO_FOOTER_PAGES = ["/login", "/register"]; // pages where the footer should be hidden
+
 export const AUTHENTICATION_OPTIONS: Array<AuthenticationOption> = [
   { name: "Email", icon: IconMail },
 ];
+
+export const CHALLENGES_MOCK_DATA: Array<ChallengeData> = [
+  {
+    title: "Tetris Challenge",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    thumbnail: "http://localhost:3000/_next/static/media/tetris.cab32540.png",
+    participants: 13,
+    creator: "Rick",
+    deadlineTimestamp: null,
+    template: "https://thispersondoesnotexist.com",
+    model: "https://thispersondoesnotexist.com",
+    dataset: "https://thispersondoesnotexist.com",
+  },
+  {
+    title: "Tetris Challenge 2",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    thumbnail: "http://localhost:3000/_next/static/media/tetris.cab32540.png",
+    participants: 4,
+    creator: "Rick",
+    deadlineTimestamp: null,
+    template: "https://thispersondoesnotexist.com",
+    model: "https://thispersondoesnotexist.com",
+    dataset: "https://thispersondoesnotexist.com",
+  },
+  {
+    title: "Challenge 3",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    thumbnail: "http://localhost:3000/_next/static/media/tetris.cab32540.png",
+    participants: 0,
+    creator: "Benny",
+    deadlineTimestamp: null,
+    template: "https://thispersondoesnotexist.com",
+    model: "https://thispersondoesnotexist.com",
+    dataset: "https://thispersondoesnotexist.com",
+  },
+  {
+    title: "New Challenge",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    thumbnail: "http://localhost:3000/_next/static/media/tetris.cab32540.png",
+    participants: 49,
+    creator: "Rick",
+    deadlineTimestamp: null,
+    template: "https://thispersondoesnotexist.com",
+    model: "https://thispersondoesnotexist.com",
+    dataset: "https://thispersondoesnotexist.com",
+  },
+  {
+    title: "Another Challenge",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    thumbnail: "http://localhost:3000/_next/static/media/tetris.cab32540.png",
+    participants: 1,
+    creator: "Rick",
+    deadlineTimestamp: null,
+    template: "https://thispersondoesnotexist.com",
+    model: "https://thispersondoesnotexist.com",
+    dataset: "https://thispersondoesnotexist.com",
+  },
+];
+
+export const LEADERBOARD_MOCK_DATA: LeaderboardData = {
+  ranking: [
+    { user: "Mochtaba", score: 20 },
+    { user: "Konrad", score: 4 },
+    { user: "Vincent", score: 19 },
+    { user: "Nick", score: 19 },
+    { user: "Matthew", score: 39 },
+  ],
+};
